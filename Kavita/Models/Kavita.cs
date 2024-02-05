10731e2b3c42efd13023d09ef9c192d54045b334
yalathiya <yashlathiya59ppsv@gmail.com>
2024-02-05 13:28:27 +0530

    Models Added

---
+++ Kavita/Models/Kavita.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kavita.Models
+{
+    /// <summary>
+    /// Class Kavita represents a model which has all details about kavita
+    /// </summary>
+    public class Kavita
+    {
+        /// <summary>
+        /// Kavita Id "PRIMARY KEY"
+        /// </summary>
+        public int kavitaId { get; set; }
+
+        /// <summary>
+        /// Title of Kavita
+        /// </summary>
+        public string title { get; set; }
+
+        /// <summary>
+        /// Content of Kavita
+        /// </summary>
+        public string content { get; set; }
+
+        /// <summary>
+        /// Date & Time when kavita is published
+        /// </summary>
+        public DateTime uploadedTime { get; set; }
+
+        /// <summary>
+        /// How many likes are given to this kavita 
+        /// </summary>
+        public long like { get; set; }
+
+        /// <summary>
+        /// Categoty of Kavita 
+        /// String.Empty is used as any kavita can be published without specifying kavita name
+        /// </summary>
+        public string category { get; set; } = string.Empty;
+    }
+}
---
+++ Kavita/Models/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kavita.Models
+{
+    /// <summary>
+    /// This class represents model of users.
+    /// </summary>
+    public class Users
+    {
+        /// <summary>
+        /// Username is "primary key" here
+        /// </summary>
+        public string username { get; set; }
+
+        /// <summary>
+        /// Email Id of the user
+        /// </summary>
+        public string email { get; set; }
+
+        /// <summary>
+        /// Password of the user
+        /// </summary>
+        public string password { get; set; }
+    }
+}
